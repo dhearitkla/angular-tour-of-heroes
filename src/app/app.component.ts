766a26bd372ad775d06d364e2b7c204c61b76ab7
DannyHearit <d.hearit@kunzleigh.com>
2024-09-06 07:06:41 -0400

    added services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent} from "./hero-detail/hero-detail.component";
+import { MessagesComponent} from "./messages/messages.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent, HeroDetailComponent],
+  imports: [RouterOutlet, HeroesComponent, HeroDetailComponent, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
